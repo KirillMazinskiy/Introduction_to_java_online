6c7350e84b6994ee096a8103f342f5bc05039d51
Kirill Mazinski <chikkahikka@mail.ru>
2020-08-07 19:32:51 +0300

    Second step! Added first task

--- 1_Basics_of_software_code_development/src/main/java/by/mazinski/App.java
+++ 1_Basics_of_software_code_development/src/main/java/by/mazinski/App.java
@@ -1,4 +1,8 @@
 package by.mazinski;
 
 public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println (MathOperations.calculateFirstTask(2.2, 3.7,3.4) );
+    }
 }
