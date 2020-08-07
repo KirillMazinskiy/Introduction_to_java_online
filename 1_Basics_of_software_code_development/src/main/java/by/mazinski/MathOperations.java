42bbb91bb1ebdc19db7f4f57efcb18252542b6ad
Kirill Mazinski <chikkahikka@mail.ru>
2020-08-07 19:37:02 +0300

    Second step! Added first task

---
+++ 1_Basics_of_software_code_development/src/main/java/by/mazinski/MathOperations.java
@@ -0,0 +1,11 @@
+package by.mazinski;
+
+public class MathOperations {
+    private int x, y, z = 0;
+    private boolean check = true;
+    public static double calculateFirstTask(double a, double b, double c){
+        double z = 0.0;
+        z = (a - 3)* b / 2 + c;
+        return z;
+    }
+}
